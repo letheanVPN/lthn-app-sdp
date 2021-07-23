344c79ce815777df9be438a006ae2850b6c0dd17
snider <snider@lethean.io>
2021-07-23 14:56:25 +0100

    adds cache interceptor for get request
    Signed-off-by: snider <snider@lethean.io>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,30 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProviderModule } from './modules/provider/provider.module';
 import { ServiceModule } from './modules/services/service.module';
 import { FavouriteModule } from './modules/favourite/favourite.module';
 import { FeedbackModule } from './modules/feedback/feedback.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
+    CacheModule.register({
+      ttl: 5, // seconds
+      max: 10, // maximum number of items in cache
+    }),
     ProviderModule,
     ServiceModule,
     FavouriteModule,
     FeedbackModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
