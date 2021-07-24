6de8ca87b18fd4cc6c5a4f09caeb62b4c03aeb84
snider <snider@lethean.io>
2021-07-24 22:03:18 +0100

    moves to .json files to configure sdk's, adds android config
    Signed-off-by: snider <snider@lethean.io>

---
+++ sdk/build/android.sh
@@ -0,0 +1,1 @@
+openapi-generator-cli generate -i ./openapi.yaml -g android -o ./sdk/client/android -c ./sdk/conf/android.json
--- sdk/conf/android.sh
+++
@@ -1,2 +0,0 @@
-openapi-generator-cli generate -i ./openapi.yaml -g android -o ./sdk/client/android \
---additional-properties=apiPackage=org.lthn.vpm,artifactId=org.lthn.vpm.client
