82259ae743fc7b37b10eb1a95530d95a1b0f2467
snider <snider@lethean.io>
2021-07-24 20:46:21 +0100

    adds android sdk git repo
    Signed-off-by: snider <snider@lethean.io>

--- sdk/conf/android.sh
+++ sdk/conf/android.sh
@@ -1,3 +1,2 @@
-openapi-generator-cli generate -i ./openapi.yaml -g android -o ../sdk/client/android \
+openapi-generator-cli generate -i ./openapi.yaml -g android -o ./sdk/client/android \
 --additional-properties=apiPackage=org.lthn.vpm,artifactId=org.lthn.vpm.client
-/bin/sh ../sdk/client/android/git_push.sh lthn.io projects/vpn/sdk/andriod "SDK Update" "gitlab.com"
