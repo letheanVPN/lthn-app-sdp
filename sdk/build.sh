26de66be20440d95f0d3caca42eb0be81523190a
snider <snider@lethean.io>
2021-07-24 22:03:18 +0100

    moves to .json files to configure sdk's, adds android config
    Signed-off-by: snider <snider@lethean.io>

--- sdk/build.sh
+++ sdk/build.sh
@@ -1,45 +1,45 @@
 rm -rf sdk/client/ruby/*
-bash sdk/conf/ruby.sh
+bash sdk/build/ruby.sh
 rm -rf sdk/client/java/*
-bash sdk/conf/java.sh
+bash sdk/build/java.sh
 rm -rf sdk/client/javascript/*
-bash sdk/conf/javascript.sh
+bash sdk/build/javascript.sh
 rm -rf sdk/client/node-ts/*
-bash sdk/conf/typescript-node.sh
+bash sdk/build/typescript-node.sh
 rm -rf sdk/client/rxjs-ts/*
-bash sdk/conf/typescript-rxjs.sh
+bash sdk/build/typescript-rxjs.sh
 rm -rf sdk/client/angular-ts/*
-bash sdk/conf/typescript-angular.sh
+bash sdk/build/typescript-angular.sh
 rm -rf sdk/client/swift5/*
-bash sdk/conf/swift5.sh
+bash sdk/build/swift5.sh
 rm -rf sdk/client/rust/*
-bash sdk/conf/rust.sh
+bash sdk/build/rust.sh
 rm -rf sdk/client/python/*
-bash sdk/conf/python.sh
+bash sdk/build/python.sh
 rm -rf sdk/client/powershell/*
-bash sdk/conf/powershell.sh
+bash sdk/build/powershell.sh
 rm -rf sdk/client/php/*
-bash sdk/conf/php.sh
+bash sdk/build/php.sh
 rm -rf sdk/client/perl/*
-bash sdk/conf/perl.sh
+bash sdk/build/perl.sh
 rm -rf sdk/client/lua/*
-bash sdk/conf/lua.sh
+bash sdk/build/lua.sh
 rm -rf sdk/client/erlang/*
-bash sdk/conf/erlang.sh
+bash sdk/build/erlang.sh
 rm -rf sdk/client/elm/*
-bash sdk/conf/elm.sh
+bash sdk/build/elm.sh
 rm -rf sdk/client/dart/*
-bash sdk/conf/dart.sh
+bash sdk/build/dart.sh
 rm -rf sdk/client/cpp-rest/*
-bash sdk/conf/cpp-rest.sh
+bash sdk/build/cpp-rest.sh
 rm -rf sdk/client/cpp-qt/*
-bash sdk/conf/cpp-qt.sh
+bash sdk/build/cpp-qt.sh
 rm -rf sdk/client/bash/*
-bash sdk/conf/bash.sh
+bash sdk/build/bash.sh
 rm -rf sdk/client/c/*
-bash sdk/conf/c.sh
+bash sdk/build/c.sh
 rm -rf sdk/client/go/*
-bash sdk/conf/go.sh
+bash sdk/build/go.sh
 rm -rf sdk/client/android/*
-bash sdk/conf/android.sh
+bash sdk/build/android.sh
 
