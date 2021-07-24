c99b65824f073424fe4124248903177f84e58e9a
snider <snider@lethean.io>
2021-07-24 15:15:11 +0100

    adds data definitions for provider feedback/stats
    Signed-off-by: snider <snider@lethean.io>

--- src/modules/vpm/feedback/feedback.controller.ts
+++ src/modules/vpm/feedback/feedback.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { FeedbackEntity } from './interfaces/feedback.entitty';
 
-@Controller('feedback')
-export class FeedbackController {}
+@ApiTags('vpm')
+@Controller({ version: '1', path: 'feedback' })
+export class FeedbackController {
+  @Post('add')
+  addFeedback(@Body() feedbackEntity: FeedbackEntity) {
+    return 'Add feedback';
+  }
+
+  @Get('get/:client/:id')
+  getProviderFeedback() {
+    return new FeedbackEntity();
+  }
+
+  @Get('stats')
+  getProvidersFeedback() {
+    return [new FeedbackEntity(), new FeedbackEntity()];
+  }
+}
