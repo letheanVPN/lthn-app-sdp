a488430d49da7aaf1a094622b4fa6a54cbfef9c1
snider <snider@lethean.io>
2021-07-24 21:45:18 +0100

    update test
    Signed-off-by: snider <snider@lethean.io>

--- sdk/build.sh
+++ sdk/build.sh
@@ -1,24 +1,45 @@
-rm -rf sdk/client
+rm -rf sdk/client/ruby/*
 bash sdk/conf/ruby.sh
+rm -rf sdk/client/java/*
 bash sdk/conf/java.sh
+rm -rf sdk/client/javascript/*
 bash sdk/conf/javascript.sh
+rm -rf sdk/client/node-ts/*
 bash sdk/conf/typescript-node.sh
+rm -rf sdk/client/rxjs-ts/*
 bash sdk/conf/typescript-rxjs.sh
+rm -rf sdk/client/angular-ts/*
 bash sdk/conf/typescript-angular.sh
+rm -rf sdk/client/swift5/*
 bash sdk/conf/swift5.sh
+rm -rf sdk/client/rust/*
 bash sdk/conf/rust.sh
+rm -rf sdk/client/python/*
 bash sdk/conf/python.sh
+rm -rf sdk/client/powershell/*
 bash sdk/conf/powershell.sh
+rm -rf sdk/client/php/*
 bash sdk/conf/php.sh
+rm -rf sdk/client/perl/*
 bash sdk/conf/perl.sh
+rm -rf sdk/client/lua/*
 bash sdk/conf/lua.sh
+rm -rf sdk/client/erlang/*
 bash sdk/conf/erlang.sh
+rm -rf sdk/client/elm/*
 bash sdk/conf/elm.sh
+rm -rf sdk/client/dart/*
 bash sdk/conf/dart.sh
+rm -rf sdk/client/cpp-rest/*
 bash sdk/conf/cpp-rest.sh
+rm -rf sdk/client/cpp-qt/*
 bash sdk/conf/cpp-qt.sh
+rm -rf sdk/client/bash/*
 bash sdk/conf/bash.sh
+rm -rf sdk/client/c/*
 bash sdk/conf/c.sh
+rm -rf sdk/client/go/*
 bash sdk/conf/go.sh
+rm -rf sdk/client/android/*
 bash sdk/conf/android.sh
 
