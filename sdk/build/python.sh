87dc2780d8dedf4b73ecc5bd21413fc125376128
snider <snider@lethean.io>
2021-07-24 22:33:49 +0100

    updates python sdk settings
    Signed-off-by: snider <snider@lethean.io>

--- sdk/build/python.sh
+++ sdk/build/python.sh
@@ -1,1 +1,1 @@
-openapi-generator-cli generate -i ./openapi.yaml -g python-legacy -o ./sdk/client/python --additional-properties=apiPackage=lethean_vpm
+openapi-generator-cli generate -i ./openapi.yaml -g python-legacy -o ./sdk/client/python -c ./sdk/conf/python.json
