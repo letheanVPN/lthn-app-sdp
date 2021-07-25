9173f304f0ebe093ac1bfca6cb48682366d9927c
snider <snider@lethean.io>
2021-07-25 17:32:33 +0100

    angular sdk for explorer
    Signed-off-by: snider <snider@lethean.io>

--- sdk/build/typescript-angular.sh
+++ sdk/build/typescript-angular.sh
@@ -1,1 +1,2 @@
-openapi-generator-cli generate -i ./openapi.yaml -g typescript-angular -o ./sdk/client/angular-ts --additional-properties=apiPackage=lethean_vpm
+openapi-generator-cli generate -i ./openapi.yaml -g typescript-angular -o ./sdk/client/angular-ts -c ./sdk/conf/angular.json
+openapi-generator-cli generate -i ./explorer.yaml -g typescript-angular -o ./sdk/explorer/angular-ts -c ./sdk/conf/angular-explorer.json
