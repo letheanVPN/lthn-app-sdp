27681e6adaba4a8df6b9c1eba8e935620e11c9a5
snider <snider@lethean.io>
2021-07-26 11:24:53 +0100

    updates sdk build script, compiles all sdk/conf/*.json files and pushes to their respective git repos
    Signed-off-by: snider <snider@lethean.io>

--- sdk/build.sh
+++ sdk/build.sh
@@ -1,45 +1,14 @@
-rm -rf sdk/client/ruby/*
-bash sdk/build/ruby.sh
-rm -rf sdk/client/java/*
-bash sdk/build/java.sh
-rm -rf sdk/client/javascript/*
-bash sdk/build/javascript.sh
-rm -rf sdk/client/node-ts/*
-bash sdk/build/typescript-node.sh
-rm -rf sdk/client/rxjs-ts/*
-bash sdk/build/typescript-rxjs.sh
-rm -rf sdk/client/angular-ts/*
-bash sdk/build/typescript-angular.sh
-rm -rf sdk/client/swift5/*
-bash sdk/build/swift5.sh
-rm -rf sdk/client/rust/*
-bash sdk/build/rust.sh
-rm -rf sdk/client/python/*
-bash sdk/build/python.sh
-rm -rf sdk/client/powershell/*
-bash sdk/build/powershell.sh
-rm -rf sdk/client/php/*
-bash sdk/build/php.sh
-rm -rf sdk/client/perl/*
-bash sdk/build/perl.sh
-rm -rf sdk/client/lua/*
-bash sdk/build/lua.sh
-rm -rf sdk/client/erlang/*
-bash sdk/build/erlang.sh
-rm -rf sdk/client/elm/*
-bash sdk/build/elm.sh
-rm -rf sdk/client/dart/*
-bash sdk/build/dart.sh
-rm -rf sdk/client/cpp-rest/*
-bash sdk/build/cpp-rest.sh
-rm -rf sdk/client/cpp-qt/*
-bash sdk/build/cpp-qt.sh
-rm -rf sdk/client/bash/*
-bash sdk/build/bash.sh
-rm -rf sdk/client/c/*
-bash sdk/build/c.sh
-rm -rf sdk/client/go/*
-bash sdk/build/go.sh
-rm -rf sdk/client/android/*
-bash sdk/build/android.sh
-
+#!/bin/bash
+# NOTE : Quote it else use array to avoid problems #
+BASE_DIR=$(dirname "$(pwd)")
+FILES=$(pwd)"/sdk/conf/*"
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  filename=$(basename "$f" .json)
+  rm -rf "$BASE_DIR"/sdk/client/"$filename"/*
+  openapi-generator-cli generate -i "$BASE_DIR"/dvpm/openapi.yaml -g "$filename" -o "$BASE_DIR"/sdk/client/"$filename" -c "$f" --git-host gitlab.com \
+  --git-repo-id projects/sdk/clients/"$filename" --git-user-id lthn.io
+  (cd "$BASE_DIR"/sdk/client/"$filename" &&  /bin/sh ./git_push.sh)
+done
