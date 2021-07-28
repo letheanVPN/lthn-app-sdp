233cb1b8b6dd2511b28e2de01da0d25fac007e5e
snider <snider@lethean.io>
2021-07-28 09:32:16 +0100

    updates build script to extract package version and to allow building a single sdk with the -p arg. the json def file needs to be made first
    Signed-off-by: snider <snider@lethean.io>

--- sdk/build.sh
+++ sdk/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 # NOTE : Quote it else use array to avoid problems #
 BASE_DIR=$(dirname "$(pwd)")
-FILES=$(pwd)"/sdk/conf/*"
+package="*"
+while getopts p: flag
+do
+    case "${flag}" in
+        p) package=${OPTARG}.json;;
+        *) ;;
+    esac
+done
+
+FILES=$(pwd)"/sdk/conf/$package"
+export PACKAGE_VERSION=$(cat "$(pwd)/package.json" \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
 for f in $FILES
 do
   echo "Processing $f file..."
@@ -9,6 +24,6 @@ do
   filename=$(basename "$f" .json)
   rm -rf "$BASE_DIR"/sdk/client/"$filename"/*
   openapi-generator-cli generate -i "$BASE_DIR"/dvpm/openapi.yaml -g "$filename" -o "$BASE_DIR"/sdk/client/"$filename" -c "$f" --git-host gitlab.com \
-  --git-repo-id projects/sdk/clients/"$filename" --git-user-id lthn.io
+  --git-repo-id projects/sdk/clients/"$filename" --git-user-id lthn.io --artifact-version "${PACKAGE_VERSION}"
   (cd "$BASE_DIR"/sdk/client/"$filename" &&  /bin/sh ./git_push.sh)
 done
